7512e82a225b659f622ff469ac59cd1d8d9157d0
Sergey Petrachkov <petrachkovsergey@gmail.com>
2020-01-15 19:30:12 +0300

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -308,3 +308,53 @@ func myAtoi(_ str: String) -> Int {
 }
 
 myAtoi("   +0 123")
+
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+  for subArray in matrix {
+    for i in 0..<subArray.count {
+      if subArray[i] > target {
+        break
+      } else {
+        if subArray[i] == target {
+          return true
+        }
+      }
+    }
+  }
+  return false
+}
+
+let matr = [
+  [1,  3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+//
+//let contains = searchMatrix(matr, 3)
+//print(contains)
+
+
+extension StringProtocol {
+  subscript(offset: Int) -> Character {
+    return self[index(startIndex, offsetBy: offset)]
+  }
+}
+//https://leetcode.com/problems/length-of-last-word/submissions/
+func lengthOfLastWord(_ s: String) -> Int {
+  return s.split(separator: " ").last?.count ?? 0
+  // or
+  var length = 0
+  var i = s.count - 1
+  while i >= 0 {
+    if s[i] != " " {
+      length += 1
+    } else {
+      if length > 0 {
+        break
+      }
+    }
+    i -= 1
+  }
+  return length
+}
+
